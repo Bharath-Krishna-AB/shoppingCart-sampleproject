4e1714a852b16c84777e10b2705beecf9340f256
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2021-04-15 14:45:33 -0700

    Login and SignUp page Almost Finshed

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 var productHelper=require('../helpers/product-helpers')
+var userHelpers=require('../helpers/userhelpers')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,6 +14,20 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
 res.render('user/login')
 });
+
+router.get('/signup', function(req, res, next) {
+  res.render('user/signup')
+  });
+
+  router.post('/signup', function(req, res, next) {
+    userHelpers.doSignup(req.body).then((response)=>{
+      console.log(response)
+    })
+    });
+
+    router.post('/login', function(req, res, next) {
+      userHelpers.doLogin(req.body)
+      });
 
 
 
