538b658283b0764edee53c549acf170feae6304e
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2021-04-15 08:58:23 -0700

    login secsion started

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   })
 });
 
-router.get('/', function(req, res, next) {
+router.get('/login', function(req, res, next) {
 res.render('user/login')
 });
 
